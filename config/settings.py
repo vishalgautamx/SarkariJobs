869726e94681e8e5e65e84a9799b061c3e7d1811
Vishal Gautam <150808958+vishalgautamx@users.noreply.github.com>
2026-09-18 04:31:31 -0700

    Fix posts app configuration

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'apps.posts',
+    'apps.core',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
